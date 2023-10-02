9a4b558b9edb26ff48228b21729b4de339786300
MartinOpat <martinopat.opat@gmail.com>
2023-10-02 11:58:39 +0200

    cakeyMcCakeFace

---
+++ Other problems/primeCount.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned ll
+#define psh push_back
+
+const ll MOD = 1000000007ll;  // 10e9+7
+
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // Looks interesting, try to solve
+
+    return 0;
+}
